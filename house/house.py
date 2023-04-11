62a5bc9f0f50c5d24e2df3a6c5c17ac5140fe06d
Val <valeriedeleon4521@gmail.com>
2023-04-11 15:29:42 -0700

    Additional methods for house

--- house/house.py
+++ house/house.py
@@ -1,3 +1,8 @@
+def main():    
+    x, y = 100, 100
+    width, height = 100, 400
+    draw_house(x, y, width, height)
+
 def draw_house(x, y, width, height):
     """
     function draws house with width and height from reference point
@@ -9,10 +14,14 @@ def draw_house(x, y, width, height):
     
     """
     print("Drawing house", x, y, width, height)
-    pass
-
-
-x, y = 100, 100
-width, height = 100, 400
-
-draw_house(x, y, width, height)
+    
+    foundation_height = 0.05 * height
+    walls_width = 0.9 * width
+    walls_height = 0.5 * height
+    roof_height = height - foundation_height - walls_height
+    
+    draw_house_foundation(x, y, width, foundation_height)
+    draw_house_walls(x, y - foundation_height, walls_width, walls_height)
+    draw_house_roof(x, y - foundation_height - walls_height, width, roof_height)
+    
+main()
