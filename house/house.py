2f511e881093582d4e41057a7ff6183e3b8b0750
Val <valeriedeleon4521@gmail.com>
2023-04-11 15:14:57 -0700

    documentation

--- house/house.py
+++ house/house.py
@@ -1,6 +1,16 @@
 def draw_house(x, y, width, height):
+    """
+    function draws house with width and height from reference point
+    
+    param x: x coordinate of the midpoint of the foundation
+    param y: y coordinate of the bottom side of the foundation
+    param width: total width of the house
+    param height: total height of the house
+    
+    """
     print("Drawing house", x, y, width, height)
     pass
+
 
 x, y = 100, 100
 width, height = 100, 400
