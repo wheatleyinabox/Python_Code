a8b1e6a64e7424bdf0545823943337aa4c52437c
Val <valeriedeleon4521@gmail.com>
2023-04-11 15:35:37 -0700

    Documentation & Method implementation

--- house/house.py
+++ house/house.py
@@ -24,4 +24,40 @@ def draw_house(x, y, width, height):
     draw_house_walls(x, y - foundation_height, walls_width, walls_height)
     draw_house_roof(x, y - foundation_height - walls_height, width, roof_height)
     
+def draw_house_foundation(x, y, width, height):
+    """
+    function draws house foundation with width and height from reference point
+    
+    param x: x coordinate of the midpoint of the foundation
+    param y: y coordinate of the bottom side of the foundation
+    param width: total width of the foundation
+    param height: total height of the foundation
+    
+    """
+    print("Drawing house foundation", x, y, width, height)
+
+def draw_house_walls(x, y, width, height):
+    """
+    function draws house walls with width and height from reference point
+    
+    param x: x coordinate of the midpoint of the foundation
+    param y: y coordinate of the bottom side of the foundation
+    param width: total width of the walls
+    param height: total height of the walls
+    
+    """
+    print("Drawing house wall", x, y, width, height)
+
+def draw_house_roof(x, y, width, height):
+    """
+    function draws house roof with width and height from reference point
+    
+    param x: x coordinate of the midpoint of the foundation
+    param y: y coordinate of the bottom side of the foundation
+    param width: total width of the roof
+    param height: total height of the roof
+    
+    """
+    print("Drawing house roof", x, y, width, height)
+    
 main()
